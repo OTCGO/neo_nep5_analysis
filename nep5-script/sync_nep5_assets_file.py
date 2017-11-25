027f7ee01dd2ff364647d20dee2f96dfe3f69b51
503945930 <503945930@qq.com>
2017-11-25 23:01:01 +0800

    update

--- nep5-script/sync_nep5_assets_file.py
+++ nep5-script/sync_nep5_assets_file.py
@@ -79,7 +79,7 @@ def get_block_index(filename):
     return os.path.splitext(blockIndex[1])
 
 
-# python sync_nep5_assets_file.py -d neo-otc -r /Users/wei/Desktop/otcgo/neo_wallet_analysis/nep5-script/test -m 127.0.0.1:27017
+# python sync_nep5_assets_file.py -d neo-otcgo -r /Users/wei/Desktop/otcgo/neo_wallet_analysis/nep5-script/test -m 127.0.0.1:27017
 
 # python sync_nep5_assets_file.py -d neo-otc -r /home/wei/Desktop/neo-work/neo-cli/Notifications -m 127.0.0.1:27017
 

94fc18107f83e0148677492ea6a1a5fe5d64bc4f
503945930 <503945930@qq.com>
2017-11-29 16:50:21 +0800

    add banaceof

--- web-server/dist/graphql/schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const graphql = require("graphql");
-const query_1 = require("./query");
-const schema = new graphql.GraphQLSchema({
-    query: query_1.default
-});
-exports.default = schema;
-//# sourceMappingURL=schema.js.map

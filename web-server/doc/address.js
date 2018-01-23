141382c22a88c583ee942151e0f93668150d25ad
503945930 <503945930@qq.com>
2018-01-23 22:23:11 +0800

    update doc

--- web-server/doc/address.js
+++ web-server/doc/address.js
@@ -9,17 +9,27 @@
 	"status": "OK",
 	"code": 200,
 	"data": [
-        {
+    {
     "_id" : ObjectId("5a45f9f094778f4e0f4ed562"),
     "updatedAt" : "1509528622",
     "createdAt" : "1509528622",
-    "contract" : "0xecc6b20d3ccac1ee9ef109af5a7cdb85706b1df9",
-    "address" : {
-        "hash" : "2afa109e291b95d86892ab2688394daa4011bf0a",
-        "value" : "AKh7bNQt9LwnrjSqRXJPEzBj537JpAqjpe"
+    "address" : AKh7bNQt9LwnrjSqRXJPEzBj537JpAqjpe,
+    "balance":[
+            {
+            "type":"" , // nep5 || utxo
+            "contract" : "0xecc6b20d3ccac1ee9ef109af5a7cdb85706b1df9",
+            "symbol":""
+            "value":""
+            },
+            {
+            "type":"" , //  utxo
+            "contract" : "0xecc6b20d3ccac1ee9ef109af5a7cdb85706b1df9",
+            "symbol":""
+            "value":""
+            }
+        ]
     }
-}
-    ],
+],
 	"server_time": 1496727468000
 }
  */
--- web-server/doc/asset.js
+++ web-server/doc/asset.js
@@ -9,23 +9,14 @@
 	"status": "OK",
 	"code": 200,
 	"data":[
-    {
-        "type": "type_string",
-        "txid": "tx_id_string",
-        "precision": integer,
-        "owner": "hash_string",
-        "name": [
         {
-            "name": "name_string",
-            "lang": "language_code_string"
-        },
-        ...
-        ],
-        "issued": float,
-        "amount": float,
-        "admin": "hash_string"
-    },
-    ...
+        "_id" : ObjectId("5a61a85a94778f7dc211a131"),
+        "symbol" : "RPX",
+        "name":""  // 中文名称
+        "createdAt" : "2018-01-19 16:12:10",
+        "contract" : "0xecc6b20d3ccac1ee9ef109af5a7cdb85706b1df9",
+        "updatedAt" : "2018-01-19 16:12:10"
+      }
     ] ,
 	"server_time": 1496727468000
 }

077c29fe72f48a703b4ec7651e654c7e160a6924
503945930 <503945930@qq.com>
2018-02-03 00:04:25 +0800

    :recycle:

--- web-server-2/deploy.sh
+++ web-server-2/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 export NVM_DIR="/home/qknow/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-cd /var/otcgo/neo_wallet_analysis/web-server
+cd /root/neo_nep5_analysis/web-server-2
 git reset --hard
 git pull origin master:master
 rm -rf dist/
 npm install
 npm run build
-count=`pm2 list|grep neo-nep5 |grep -v grep |wc -l`
+count=`pm2 list|grep neo-api |grep -v grep |wc -l`
 if [ $count -eq 0 ];then
 pm2 start pm2.prod.config.js 
 else
--- web-server-2/pm2.prod.config.js
+++ web-server-2/pm2.prod.config.js
@@ -5,7 +5,7 @@ module.exports = {
      */
   apps: [
     {
-      name: 'neo-nep5',
+      name: 'neo-api',
       script: './dist/app.js',
       env: {   // all environment
         'NODE_ENV': 'production'

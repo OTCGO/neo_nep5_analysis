825a93189bd5b9aa9800eccc060cad625e7e3445
503945930 <503945930@qq.com>
2018-03-06 20:45:12 +0800

    fix deply

--- web-server/deploy.sh
+++ web-server/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 export NVM_DIR="/home/qknow/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-cd /var/otcgo/neo_wallet_analysis/web-server
 git reset --hard
 git pull origin master:master
 rm -rf dist/
